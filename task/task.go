3abe30cf5a1286f29584639f45ab823e16fa18a1
Marc Capell <marc.capell@gmail.com>
2024-12-20 14:26:53 +0100

    refactor: move file-management to tasks

--- task/task.go
+++ task/task.go
@@ -3,7 +3,10 @@ package task
 import (
 	"context"
 	"fmt"
+	"io"
 	"log/slog"
+	"os"
+	"path/filepath"
 
 	"github.com/mcapell/llmreview/llm"
 	"github.com/mcapell/llmreview/llm/types"
@@ -36,7 +39,7 @@ func (t *Task) Run(ctx context.Context) error {
 
 		for _, q := range questions {
 			slog.Debug(fmt.Sprintf("processing question: %s using model %s", q.Path, cli))
-			if question.ResultExists(t.ResultPath, q) {
+			if t.resultExist(q) {
 				slog.Debug("result already exist; ignoring question")
 				continue
 			}
@@ -46,10 +49,40 @@ func (t *Task) Run(ctx context.Context) error {
 				return fmt.Errorf("error from %s: %w", model, err)
 			}
 
-			if err := question.StoreResult(t.ResultPath, q, response); err != nil {
+			if err := t.storeResult(q, response); err != nil {
 				return fmt.Errorf("error storing result: %w", err)
 			}
 		}
+	}
+
+	return nil
+}
+
+func (t *Task) resultExist(q question.Question) bool {
+	f, err := os.Open(t.ResultPath + q.Path)
+	if err != nil {
+		return false
+	}
+	defer f.Close()
+
+	return true
+
+}
+
+func (t *Task) storeResult(q question.Question, result string) error {
+	p := t.ResultPath + q.Path
+	if err := os.MkdirAll(filepath.Dir(p), os.ModePerm); err != nil {
+		return fmt.Errorf("error creating the results path: %w", err)
+	}
+
+	f, err := os.Create(p)
+	if err != nil {
+		return fmt.Errorf("error creating result file: %w", err)
+	}
+	defer f.Close()
+
+	if _, err := io.WriteString(f, result); err != nil {
+		return fmt.Errorf("error writing result: %w", err)
 	}
 
 	return nil
